e4eff4928d6218c1ba63649fc39d89a2f30fcbd9
Ramsharan Shrestha <ramsharan065@gmail.com>
2015-06-11 16:09:02 +0545

    render always pass response of idology

--- grails-app/controllers/com/test/TestController.groovy
+++ grails-app/controllers/com/test/TestController.groovy
@@ -19,8 +19,21 @@ class TestController {
     }
 
     def idology(){
-        def data = Idology.last()
-        render data?.response
+        //def data = Idology.last()
+        //render data?.response
+        render '''<?xml version="1.0"?>
+<response>
+    <id-number>1124550715</id-number>
+    <summary-result>
+        <key>id.success</key>
+        <message>PASS</message>
+    </summary-result>
+    <results>
+        <key>result.match</key>
+        <message>ID Located</message>
+    </results>
+    <id-scan>no</id-scan>
+</response>'''
     }
 
     def idologyAnswerResponse(){
